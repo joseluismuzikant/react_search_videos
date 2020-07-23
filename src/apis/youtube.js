d87b428a422b665bc81956c6b020913e99112310
Luis Muzikant <luis.muzikant@lighthousepmg.com>
2020-07-23 15:06:10 -0300

    YouTube play list using React.js

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const KEY='AIzaSyDUA4zP4FCfUgM8qAbBamRrA0dZthUZVq8';
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params:{
+      part:'snippet',
+      type:'video',
+      maxResults:5,
+      key:KEY
+  }
+});
